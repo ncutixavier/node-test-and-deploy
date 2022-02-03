14bb2d9c4789b72d1e7e2241741b5fa300132fd1
Ncuti Xavier <51375428+ncutixavier@users.noreply.github.com>
2022-02-03 14:52:07 +0200

    Configure continuous integration (#1)
    * ft:configure ci
    
    * add more test

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # node-test-and-deploy
+![example workflow](https://github.com/ncutixavier/node-test-and-deploy/actions/workflows/node.js.yml/badge.svg)
--- tests/articles.test.js
+++ tests/articles.test.js
@@ -7,12 +7,39 @@ use(chaiHttp)
 
 describe("ARTICLE END-POINT TESTING", () => {
     it("Should retrieve the articles", async () => {
-        const res = await request(app).get("/api/v1/aritcles/")
+        const res = await request(app).get("/api/v1/articles/")
         expect(res).to.have.status([200])
         expect(res.type).to.have.equal("application/json")
     })
+
     it("Should not retrieve the articles", async () => {
         const res = await request(app).get("/api/v1/aritcle/")
         expect(res).to.have.status([404])
     })
+
+    it("Should retrieve the article by ID", async () => {
+      const res = await request(app).get(
+        "/api/v1/articles/61fae837852d90ca8358d7bf"
+      );
+      expect(res).to.have.status([200]);
+    });
+
+    it("Should not create an article", (done) => {
+      request(app)
+        .post("/api/v1/articles")
+        .send({
+          title: "test",
+          content: "test",
+          image: "test",
+        })
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(401);
+          done();
+        });
+    });
+
+    it("Should not retrieve the articles", async () => {
+      const res = await request(app).get("/api/v1/articles/3434");
+      expect(res).to.have.status([404]);
+    });
 })
